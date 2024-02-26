0c5c1fc4325d8d612b3128bcf4be0569c696cab5
Alexis <alexishadjian@gmail.com>
2024-02-26 10:23:06 +0100

    [Home] Top layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,15 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello world</h1>
+      <span>Jeudi, 21 avr</span>
+      <div className={styles.top + " flex space"}>
+        <div className="col4">
+          <h1>Qui travaille aujourd’hui ?</h1>
+        </div>
+        <div>
+          menu
+        </div>
+      </div>
     </main>
   );
 }
