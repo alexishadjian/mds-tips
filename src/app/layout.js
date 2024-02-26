338033164a76e3c35008f8c7b8c1b199eea094bf
Alexis <alexishadjian@gmail.com>
2024-02-26 10:03:21 +0100

    Darkmode system, color variables

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+import Theme from './components/Theme'
 import "../styles/global.scss";
+
 
 
 export const metadata = {
@@ -9,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+        <Theme>
+          <body>{children}</body>
+        </Theme>
     </html>
   );
 }
